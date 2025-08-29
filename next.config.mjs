bd35368ec8f23eaf3b801dbe6bba7690c1788b26
vacillus <zepeda.samuel93@gmail.com>
2025-08-28 17:16:06 -0700

    Fix Next.js config for static export to Amplify

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Temporarily remove 'export' to generate server files
-  // output: 'export',
+  output: 'export',
   images: {
     unoptimized: true
   },
-  distDir: '.next',
-  trailingSlash: true,
-  experimental: {
-    outputFileTracing: true,
-    outputFileTracingRoot: process.cwd(),
-    outputStandalone: true
-  }
+  trailingSlash: true
 };
 
 export default nextConfig;
